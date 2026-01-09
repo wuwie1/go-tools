50578632a7ad319547c3c6d9fbb2bf95d97b50d5
likai <kaili@deepglint.com>
2026-01-09 17:21:22 +0800

    feat: env

--- env/env.go
+++ env/env.go
@@ -13,3 +13,15 @@ func GetLLMModel() string {
 func GetModelApiKey() string {
 	return os.Getenv("LLM_API_KEY")
 }
+
+func GetGMLModelBaseUrl() string {
+	return os.Getenv("MODEL_BASE_GLM_URL")
+}
+
+func GetGMLLLMModel() string {
+	return os.Getenv("LLM_GML_API_KEY")
+}
+
+func GetGMLModelApiKey() string {
+	return os.Getenv("LLM_GML_MODEL")
+}
